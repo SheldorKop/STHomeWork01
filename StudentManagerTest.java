15be5672e9e7c0e56da96fe5252162a3149d531a
SheldorKop <61685791+SheldorKop@users.noreply.github.com>
2021-04-25 19:35:44 +0800

    Update StudentManagerTest.java

--- StudentManagerTest.java
+++ StudentManagerTest.java
@@ -3,18 +3,10 @@ package STHomework01;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
- * 	1 定义学生类
- * 	2 主页面编写
- * 	3 查看学生信息
- * 	4 添加学生信息
- * 	5 删除学生
- * 	6 修改学生信息
- */
 public class StudentManagerTest {
     public static void main(String[] args) {
         ArrayList<Student> array=new ArrayList<Student>();
-        /* 这是主界面 */
+
         while (true) {
             System.out.println("请选择操作:");
             System.out.println("***********************************");
@@ -44,12 +36,10 @@ public class StudentManagerTest {
                     findAllStudent(array);
                     break;
                 case "6":
-                    /*
-                     * case穿透 System.out.println("谢谢你的使用"); break;
-                     */
+
                 default:
-                    System.out.println("谢谢使用");
-                    System.exit(0);//jvm退出
+                    System.out.println("end");
+                    System.exit(0);
                     break;
             }
         }
@@ -59,21 +49,29 @@ public class StudentManagerTest {
             System.out.println("对不起,目前没有学生信息可供查询,请回去重新操作");
             return;
         }
-        System.out.println("学号\t姓名\t年龄\t性别");
+        System.out.println("学号\t姓名\t出生日期\t性别");
         for(int i=0;i<array.size();i++) {
             Student s=array.get(i);
             System.out.println(s.getId() + "\t" + s.getName() + "\t" + s.getAge() + "\t" + s.getAddress());
         }
     }
     public static void findStudent(ArrayList<Student> array) {
-        if(array.isEmpty()) {
-            System.out.println("对不起,目前没有学生信息可供查询,请回去重新操作");
-            return;
-        }
-        System.out.println("学号\t姓名\t年龄\t性别");
+        Scanner scanner=new Scanner(System.in);
+        boolean flag=false;
+        System.out.println("请输入你要搜索学生的姓名:");
+        String name=scanner.nextLine();
+        int index=-1;
         for(int i=0;i<array.size();i++) {
-            Student s=array.get(i);
-            System.out.println(s.getId() + "\t" + s.getName() + "\t" + s.getAge() + "\t" + s.getAddress());
+            Student student=array.get(i);
+            if(student.getName().equals(name)) {
+                System.out.println("学号\t姓名\t出生日期\t性别");
+                Student s=array.get(i);
+                System.out.println(s.getId() + "\t" + s.getName() + "\t" + s.getAge() + "\t" + s.getAddress());
+                flag=true;
+            }
+        }
+        if(flag==false) {
+            System.out.println("不好意思,你要查询的学生信息不存在,请重新选择操作");
         }
     }
     public static void addStudent(ArrayList<Student> array) {
@@ -82,7 +80,6 @@ public class StudentManagerTest {
         while(true) {
             System.out.println("请输入学生学号: ");
             id=scanner.nextLine();
-            // 判断学号有没有被占用
             boolean flag=false;
             for(int i=0;i<array.size();i++) {
                 Student student=array.get(i);
@@ -100,7 +97,7 @@ public class StudentManagerTest {
         }
         System.out.println("请输入学生名字:");
         String name=scanner.nextLine();
-        System.out.println("请输入学生年龄: ");
+        System.out.println("请输入学生出生日期: ");
         String age=scanner.nextLine();
         System.out.println("请输入学生性别(homme or femme): ");
         String address=scanner.nextLine();
@@ -115,18 +112,18 @@ public class StudentManagerTest {
     public static void deleteStudent(ArrayList<Student> array) {
         Scanner scanner=new Scanner(System.in);
         boolean flag=false;
-        System.out.println("请输入你要删除学生的学号:");
-        String id=scanner.nextLine();
+        System.out.println("请输入你要删除学生的姓名:");
+        String name=scanner.nextLine();
         for(int i=0;i<array.size();i++) {
             Student student=array.get(i);
-            if(student.getId().equals(id)) {
+            if(student.getName().equals(name)) {
                 array.remove(i);
                 flag=true;
                 System.out.println("删除学生成功");
             }
         }
         if(flag==false) {
-            System.out.println("不好意思,你要删除学号的学生的信息不存在,请回去重新选择你的操作");
+            System.out.println("不好意思,你要删除的学生信息不存在,请重新选择操作");
         }
     }
     public static void updateStudent(ArrayList<Student> array) {
@@ -143,9 +140,9 @@ public class StudentManagerTest {
             }
         }
         if(flag==false) {
-            System.out.println("不好意思,你要修改学号对应的学生信息不存在,请回去重新选择你的操作");
+            System.out.println("你要修改的学生信息不存在,请重新选择操作");
         }else {
-            System.out.println("请输入新的学生年龄: ");
+            System.out.println("请输入新的学生出生日期: ");
             String age=scanner.nextLine();
             System.out.println("请输入新的学生名字:");
             String name=scanner.nextLine();
